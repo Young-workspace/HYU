b2d070e952157cf808449c08d301d3186688fc97
Young-workspace <82203285+Young-workspace@users.noreply.github.com>
2021-04-10 00:07:32 +0900

    HW1 - Insertion Sort

---
+++ 2학년/알고리즘 및 문제해결 기법/과제1/Insertion.cpp
@@ -0,0 +1,47 @@
+// 2017030155 이채영
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+
+
+void Insertion(int arr[], int size)
+{
+	int i, j;
+	for( i = 1; i<size; i++)
+	{
+		 j = i;
+		while(j>0 && arr[j-1] < arr[j]){
+			swap(arr[j-1], arr[j]);
+			j--;
+		}
+	}
+}
+
+int main()
+{
+	int N,  i, j;
+	
+	cin >> N;
+	
+	int *arr = new int[N];
+	
+	for(i = 0; i<N; i++)
+	{
+		cin>>arr[i];
+	}
+	
+	Insertion(arr,N);
+	
+	for(j = 0; j<N; j++)
+	{
+		cout<<arr[j]<<"\n";
+	}
+	
+	
+	delete []arr;
+	
+	return 0;
+	
+}
+
